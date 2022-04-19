35a676bf2dd67274b90914825d92f0e8da57a077
Michael Sanabria <maicolsana12@gmail.com>
2022-04-19 09:23:51 -0500

    Optimizing dependencies weight

--- src/Components/productCart.js
+++ src/Components/productCart.js
@@ -36,12 +36,7 @@ function ProductCart({ title, image, id }) {
   };
 
   return (
-    <motion.div
-      className="productCart my-4"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    >
+    <motion.div className="productCart my-4" animate={{ opacity: 1 }}>
       <img src={image} alt={title}></img>
       <h5 className="titleProductCart">{title}</h5>
       <Row className="mt-4">
